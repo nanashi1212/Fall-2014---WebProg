e2e5b81da1d58ff5a538ae0300b823851d89183e
unknown <inglesec1@LC112PC10.active.newpaltz.edu>
2014-09-08 10:20:23 -0400

    Index Added

---
+++ playground/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>
+				HTML
+		</title>
+	</head>
+	<body>
+		<div>
+			<header>
+				<h1>
+					Aptana Workspace Project
+				</h1>
+			</header>
+		</div>
+	</body>
+</html>
